14e0a1b799dc916fc6b9533c0e34612b8e45c9df
r.gabdrashitov <resha_goblin@mail.ru>
2022-09-05 22:04:07 +0500

    component form, emit

---
+++ src/components/Notes/Form.vue
@@ -0,0 +1,46 @@
+<!-- @format -->
+
+<template>
+	<div class="note-form-wrapper">
+		<form class="note-form" @submit.prevent="onSubmit">
+			<textarea
+				type="text"
+				required
+				placeholder="Type ur note"
+				v-model="value"
+			/>
+			<button class="btn btnPrimary" type="submit">Add new note</button>
+		</form>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			value: ''
+		}
+	},
+
+	methods: {
+		onSubmit() {
+			this.$emit('onSubmit', this.value)
+			this.value = ''
+		}
+	}
+}
+</script>
+
+<style lng="scss">
+.note-form-wrapper {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+.note-form {
+	display: flex;
+	flex-direction: column;
+	max-width: 600px;
+	width: 100%;
+}
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,5 +1,20 @@
 <!-- @format -->
 
 <template>
-	<p>home url</p>
+	<Form @onSubmit="handleSubmit" />
 </template>
+
+<script>
+import Form from '@/components/Notes/Form.vue'
+
+export default {
+	components: {
+		Form
+	},
+	methods: {
+		handleSubmit(note) {
+			console.log(note)
+		}
+	}
+}
+</script>
